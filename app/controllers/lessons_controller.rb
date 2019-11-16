91c81083e9358c45c60f58a8a95e8f38fe5e5b8e
Joe <joeholley@me.com>
2019-11-16 16:43:53 +0000

    backup troubleshooting lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section, only: [:show]
+
   def show
   end
 
@@ -7,5 +10,11 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+  
+  def require_authorized_for_current_section
+    if current_user.enrolled_in? presence: true
+      redirect_to course_path(current_lesson.section.course), alert: "Enroll to view lesson!"
+    end
   end
 end
